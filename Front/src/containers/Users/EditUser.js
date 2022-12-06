844cc0dc91a5b8ee2eae82e07447eae6912dd6da
alseny-diallo <alsenydiallo0409@gmail.com>
2022-12-06 21:49:51 +0000

    style: edit form component

--- Front/src/containers/Users/EditUser.js
+++ Front/src/containers/Users/EditUser.js
@@ -1,14 +1,33 @@
 import { useTranslation } from 'react-i18next';
-import { modifyUser, getUser } from "../../api/users";
-import { useAsync } from "react-async";
+import { modifyUser, getUser } from '../../api/users';
+import { useAsync } from 'react-async';
 import { useFormik } from 'formik';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
-import {useParams} from "react-router";
+import { useParams } from 'react-router';
 import styled from 'styled-components';
 
 const FieldContainer = styled.div`
   margin-bottom: 15px;
+  width: 50%;
+`;
+const Button = styled.button`
+  background: #1565c0;
+  color: white;
+  font-size: 1rem;
+  padding: 0.8rem 2rem;
+  border: 2px solid #1565c0;
+  border-radius: 5px;
+`;
+
+const Form = styled.form`
+  display: grid;
+  place-items: center;
+  margin: 5rem 18rem;
+  padding: 4rem 1rem;
+  background: rgb(255, 255, 255);
+  border: 2px solid rgb(219, 229, 230);
+  border-radius: 1rem;
 `;
 
 const EditUser = () => {
@@ -16,51 +35,62 @@ const EditUser = () => {
   const { userId } = useParams();
   const { data } = useAsync({ promiseFn: getUser, userId });
   const navigate = useNavigate();
-  const user = data?.data || {login: '',
-    email: '',password:''};
+  const user = data?.data || { login: '', email: '', password: '' };
   const formik = useFormik({
     initialValues: user,
     enableReinitialize: true,
     onSubmit: async (values) => {
-      await modifyUser(user.id,values);
+      await modifyUser(user.id, values);
       navigate('/users');
     },
   });
   return (
-        <form onSubmit={formik.handleSubmit}>
-          <FieldContainer>
-            <TextField id="login" name="login" label={t('LOGIN')} variant="standard"
-                       value={formik.values.login}
-                       onChange={formik.handleChange}
-                       error={formik.touched.login && Boolean(formik.errors.login)}
-                       helperText={formik.touched.login && formik.errors.login}
-                       />
-          </FieldContainer>
+    <Form onSubmit={formik.handleSubmit}>
+      <FieldContainer>
+        <TextField
+          id="login"
+          name="login"
+          label={t('LOGIN')}
+          variant="outlined"
+          value={formik.values.login}
+          onChange={formik.handleChange}
+          error={formik.touched.login && Boolean(formik.errors.login)}
+          helperText={formik.touched.login && formik.errors.login}
+          fullWidth
+        />
+      </FieldContainer>
 
-          <FieldContainer>
-            <TextField id="email" name="email" type="email" label={t('EMAIL')} variant="standard"
-                       value={formik.values.email}
-                       onChange={formik.handleChange}
-                       error={formik.touched.email && Boolean(formik.errors.email)}
-                       helperText={formik.touched.email && formik.errors.email}/>
-          </FieldContainer>
+      <FieldContainer>
+        <TextField
+          id="email"
+          name="email"
+          type="email"
+          label={t('EMAIL')}
+          variant="outlined"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          fullWidth
+        />
+      </FieldContainer>
 
-          <FieldContainer>
-            <TextField
-              id="password"
-              name="password"
-              label={t('PASSWORD')}
-              variant="standard"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-          </FieldContainer>
+      <FieldContainer>
+        <TextField
+          id="password"
+          name="password"
+          label={t('PASSWORD')}
+          variant="outlined"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          fullWidth
+        />
+      </FieldContainer>
 
-
-          <button type="submit">{t('SUBMIT')}</button>
-        </form>
+      <Button type="submit">{t('SUBMIT')}</Button>
+    </Form>
   );
 };
 
