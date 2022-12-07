3ce6ca1a099a6a0d8db8be0248546869459d680d
alseny-diallo <alsenydiallo0409@gmail.com>
2022-12-07 19:27:23 +0000

    api: add user method

--- Front/src/api/users.js
+++ Front/src/api/users.js
@@ -1,10 +1,9 @@
 import { api } from './';
 
-export const getUsers = () =>
-    api.get(`/api/v1/users`);
+export const getUsers = () => api.get(`/api/v1/users`);
 
-export const modifyUser = (id,payload) =>
-    api.put(`/api/v1/users/${id}`,payload);
+export const modifyUser = (id, payload) =>
+  api.put(`/api/v1/users/${id}`, payload);
 
-export const getUser = ({userId}) =>
-    api.get(`/api/v1/users/${userId}`);
+export const getUser = ({ userId }) => api.get(`/api/v1/users/${userId}`);
+export const addUser = (payload) => api.post(`/api/v1/users`, payload);
