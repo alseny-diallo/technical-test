ba860af0656e82b473ee8a5d3a5d3963bc28116d
alseny-diallo <alsenydiallo0409@gmail.com>
2022-12-08 09:59:11 +0000

    style: user profil

--- Front/src/containers/Users/ViewUser.js
+++ Front/src/containers/Users/ViewUser.js
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import { getUser } from "../../api/users";
-import { useAsync } from "react-async";
-import {useParams} from "react-router";
+import { getUser } from '../../api/users';
+import { useAsync } from 'react-async';
+import { useParams } from 'react-router';
 import styled from 'styled-components';
+import { Avatar, Card, CardContent, CardHeader } from '@mui/material';
 
 const FieldContainer = styled.div`
-  margin-bottom: 15px;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
 `;
 
 const ViewUser = () => {
@@ -13,26 +15,30 @@ const ViewUser = () => {
   const { userId } = useParams();
   const { data } = useAsync({ promiseFn: getUser, userId });
   const user = data?.data;
+  const logo = `${user?.login}`.charAt(0);
   return (
-        <>
-          <FieldContainer>
-            <div>
-              {t('LOGIN')} : {user?.login}
-            </div>
-          </FieldContainer>
+    <Card sx={{ m: 2 }}>
+      <CardHeader
+        titleTypographyProps={{ variant: 'h6' }}
+        avatar={
+          <Avatar sx={{ bgcolor: 'orangered' }}>{logo.toUpperCase()}</Avatar>
+        }
+        title={user?.login}
+      />
+      <CardContent>
+        <FieldContainer>
+          <div>
+            {t('EMAIL')} : {user?.email}
+          </div>
+        </FieldContainer>
 
-          <FieldContainer>
-            <div>
-              {t('EMAIL')} : {user?.email}
-            </div>
-          </FieldContainer>
-
-          <FieldContainer>
-            <div>
-              {t('PASSWORD')} : {user?.password}
-            </div>
-          </FieldContainer>
-        </>
+        <FieldContainer>
+          <div>
+            {t('PASSWORD')} : {user?.password}
+          </div>
+        </FieldContainer>
+      </CardContent>
+    </Card>
   );
 };
 
